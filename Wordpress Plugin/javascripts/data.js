bc28dd4583de3af72f278f3c44a71a9ee6c77ddf
massimiliano-romagnoli <68606608+massimiliano-romagnoli@users.noreply.github.com>
2020-07-30 16:21:38 +0200

    WORDPRESS PLUGIN 1.0

---
+++ Wordpress Plugin/javascripts/data.js
@@ -0,0 +1,51 @@
+let myArray = {
+  "name": "MAIN",
+  "video": "https://www.youtube.com/embed/lx9qeRLUYs8",
+  "paragraph": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  "children": [{
+      "name": "Par 1",
+      "video": "https://www.youtube.com/embed/FeJKJ5MoCHY",
+      "paragraph": "Voluptatem quidem, Brute, si ipsa pro se loquatur nec tam pertinaces habeat patronos, concessuram arbitror convictam superiore libro dignitati. etenim sit inpudens, si virtuti diutius repugnet, aut si honestis iucunda anteponat aut pluris esse contendat dulcedinem corporis ex eave natam laetitiam quam gravitatem animi atque constantiam. quare illam quidem dimittamus et suis se finibus tenere iubeamus, ne blanditiis eius inlecebrisque impediatur disputandi severitas.",
+      "children": [{
+          "name": "Sub 1.1",
+          "video": "https://www.youtube.com/embed/UbDeqPdUek",
+          "paragraph": "Quamquam ex omnibus philosophis Stoici plurima novaverunt, Zenoque, eorum princeps, non tam rerum inventor fuit quam verborum novorum. quodsi in ea lingua, quam plerique uberiorem putant, concessum a Graecia est ut doctissimi homines de rebus non pervagatis inusitatis verbis uterentur, quanto id nobis magis est concedendum, qui ea nunc primum audemus attingere? et quoniam saepe diximus, et quidem cum aliqua querela non Graecorum modo, sed eorum etiam, qui se Graecos magis quam nostros haberi volunt.",
+          "size": 45
+     }, {
+          "name": "Sub 1.2",
+          "video": "https://www.youtube.com/embed/RPekEt1Kjg8",
+          "paragraph": "Nam in Tusculano cum essem vellemque e bibliotheca pueri Luculli quibusdam libris uti, veni in eius villam, ut eos ipse, ut solebam, depromerem. quo cum venissem, M. Catonem, quem ibi esse nescieram, vidi in bibliotheca sedentem multis circumfusum Stoicorum libris. erat enim, ut scis, in eo aviditas legendi, nec satiari poterat, quippe qui ne reprehensionem quidem vulgi inanem reformidans.",
+          "size": 55
+     }]
+  }, {
+      "name": "Par 2",
+      "video": "https://www.youtube.com/embed/UNroEwFxh6I",
+      "paragraph": "Zenoque, eorum princeps, non tam rerum inventor fuit quam verborum novorum. quodsi in ea lingua, quam plerique uberiorem putant, concessum a Graecia est ut doctissimi homines de rebus non pervagatis inusitatis verbis uterentur, quanto id nobis magis est concedendum, qui ea nunc primum audemus attingere? et quoniam saepe diximus, et quidem cum aliqua querela non Graecorum modo, sed eorum etiam, qui se Graecos magis quam nostros haberi volunt.",
+      "children": [{
+          "name": "Sub 2.1",
+          "video": "https://www.youtube.com/embed/K_8B4bcrSs8",
+          "paragraph": "Heri, inquam, ludis commissis ex urbe profectus veni ad vesperum. causa autem fuit huc veniendi ut quosdam hinc libros promerem. et quidem, Cato, hanc totam copiam iam Lucullo nostro notam esse oportebit; nam his libris eum malo quam reliquo ornatu villae delectari. est enim mihi magnae curae—quamquam hoc quidem proprium tuum munus est—, ut ita erudiatur, ut et patri et Caepioni nostro et tibi tam propinquo respondeat.",
+          "size": 30
+     }, {
+          "name": "Sub 2.2",
+          "video": "https://www.youtube.com/embed/RJra0fcMsVU",
+          "paragraph": "Egone quaeris, inquit, quid sentiam? quos bonos viros, fortes, iustos, moderatos aut audivimus in re publica fuisse aut ipsi vidimus, qui sine ulla doctrina naturam ipsam secuti multa laudabilia fecerunt, eos melius a natura institutos fuisse, quam institui potuissent a philosophia, si ullam aliam probavissent praeter eam, quae nihil aliud in bonis haberet nisi honestum, nihil nisi turpe in malis; ceterae philosophorum disciplinae, omnino alia magis alia, sed tamen omnes.",
+          "size": 70
+     }]
+  }, {
+      "name": "Par 3",
+      "video": "https://www.youtube.com/embed/f1dnocPQXDQ",
+      "paragraph": "Facillimum id quidem est, inquam. si enim Zenoni licuit, cum rem aliquam invenisset inusitatam, inauditum quoque ei rei nomen inponere, cur non liceat Catoni? nec tamen exprimi verbum e verbo necesse erit, ut interpretes indiserti solent, cum sit verbum, quod idem declaret, magis usitatum. equidem soleo etiam quod uno Graeci, si aliter non possum, idem pluribus verbis exponere. et tamen puto concedi nobis oportere ut Graeco verbo utamur.",
+      "children": [{
+          "name": "Sub 3.1",
+          "video": "https://www.youtube.com/embed/jrc4aOfu7aQ",
+          "paragraph": "Videamus nunc, quam sint praeclare illa his, quae iam posui, consequentia. cum enim hoc sit extremum —sentis enim, credo, me iam diu, quod tšlow Graeci dicant, id dicere tum extremum, tum ultimum, tum summum; licebit etiam finem pro extremo aut ultimo dicere—, cum igitur hoc sit extremum, congruenter naturae convenienterque vivere, necessario sequitur omnes sapientes semper feliciter, absolute, fortunate vivere, nulla re impediri.",
+          "size": 40
+     }, {
+          "name": "Sub 3.2",
+          "video": "https://www.youtube.com/embed/EVJ_sbmw07M",
+          "paragraph": "Quod est bonum, omne laudabile est; quod autem laudabile est, omne est honestum; bonum igitur quod est, honestum est. satisne hoc conclusum videtur? certe; quod enim efficiebatur ex iis duobus, quae erant sumpta, in eo vides esse conclusum. duorum autem, e quibus effecta conclusio est, contra superius dici solet non omne bonum esse laudabile. nam quod laudabile sit honestum esse conceditur.",
+          "size": 60
+     }]
+  }]
+}
---
+++ Wordpress Plugin/javascripts/main.js
@@ -0,0 +1,202 @@
+// DATA
+function getData() {
+         return myArray; // change data in data.js
+    }
+
+dataset = getData();
+
+// COLORS
+var colorArray = ["#0026b1", "#4e9912", "#d826ac", "#00faff", "#e10078", "#6affff", "#003891", "#f8d891", "#007afd", "#003f00", "#ffdcff", "#003e51", "#ffb0a5", "#fac7e7", "#f4d8d4"];
+var color = d3.scale.ordinal().range(colorArray);
+
+// CHART SIZE
+var width = 650,
+    height = 650,
+    radius = Math.min(width / 2, height / 2) * 0.90;
+
+// SCALES (ricordati le interpolation alla fine)
+var x = d3.scale.linear()
+.range([0, 2 * Math.PI]);
+
+var y = d3.scale.linear()
+.range([0, radius]);
+
+// SVG VALUES AND ATTRIBUTES
+var divHeight = height;
+var divWidth = width;
+
+var svg = d3.select("#chart").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(" + divWidth / 2 + "," + height / 2 + ") rotate(-90 0 0)");
+
+// PARTITION
+var partition = d3.layout.partition()
+    .value(function(d) {
+        return d.size;
+      });
+
+// CHART STRUCTURE
+
+var tooltip = d3.select("body")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("position", "absolute")
+    .style("z-index", "10")
+    .style("opacity", 0);
+
+var arc = d3.svg.arc()
+    .startAngle(function(d) {
+        if (d.size != 0)
+        return Math.max(0, Math.min(2 * Math.PI, x(d.x)));
+      })
+    .endAngle(function(d) {
+
+        if (d.depth == 0)
+        return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx)));
+
+        if (d.depth == 2) {
+        return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx)));
+      }
+
+        if (d.size != 0)
+        return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx)));
+      })
+    .innerRadius(function(d) {
+        if (d.size != 0)
+        return Math.max(0, y(d.y));
+      })
+    .outerRadius(function(d) {
+        if (d.size != 0)
+        return Math.max(0, y(d.y + d.dy));
+
+      });
+
+var root = dataset;
+
+// DATA ONCLICK/OVER/MOVE
+var g = svg.selectAll("g")
+    .data(partition.nodes(root))
+    .enter().append("g");
+
+var path = g.append("path")
+    .attr("d", arc)
+    .style("fill", function(d) {
+        return color((d.children ? d : d.parent).name);
+      })
+    .on("click", click) // see the function below
+    .on("mouseover", function(d) {
+        tooltip.html(function() {
+        return d.name + "<br>" + d.value;
+      });
+        return tooltip.transition()
+    .duration(50)
+    .style("opacity", 0.9);
+      })
+    .on("mousemove", function(d) {
+        return tooltip
+    .style("top", (d3.event.pageY-10)+"px")
+    .style("left", (d3.event.pageX+10)+"px");
+      })
+    .on("mouseout", function(){return tooltip.style("opacity", 0);});
+
+
+//.append("text")
+var text = g.append("text")
+    .attr("x", function(d) {
+        return y(d.y);
+      })
+    .attr("dx", function(d) {
+        return d.depth == 0 ? "9" : "9"
+      }) // margin
+    .attr("dy", ".35em") // align vert
+    .attr("transform", function(d) {
+        return "rotate(" + computeTextRotation(d) + ")";
+      })
+    .text(function(d) {
+        return d.size == null ? d.name : d.name;
+      })
+    .style("fill", "black");
+
+function computeTextRotation(d) {
+      var angle = x(d.x + d.dx / 2) - Math.PI / 2;
+      return angle / Math.PI * 180;
+    }
+
+// RECTANGULAR SECTION CONTENT ONCLICK
+function click(d) {
+      document.getElementById("articleHeader").innerText = d.name;
+      document.getElementById("articlePar").innerText = d.paragraph;
+      document.getElementById("articleVideo").src = d.video;
+      // fading
+      if (d.size !== undefined) {
+          d.size += 100;
+    };
+
+// TEXT LABELS
+text.transition().attr("opacity", 0);
+
+path.transition()
+.duration(750)
+.attrTween("d", arcTween(d))
+.each("end", function(e, i) {
+        if (e.x >= d.x && e.x < (d.x + d.dx)) {
+            // associated text
+        var arcText = d3.select(this.parentNode).select("text");
+            // recalculate text position
+            arcText.transition().duration(750)
+                   .attr("opacity", 1)
+                   .attr("transform", function() {
+              return "rotate(" + computeTextRotation(e) + ")"
+            })
+                   .attr("x", function(d) {
+              return y(d.y);
+            });
+        }
+    });
+};
+
+// WORD WRAP
+var insertLinebreaks = function(t, d, width) {
+       var el = d3.select(t);
+       var p = d3.select(t.parentNode);
+       p.append("g")
+        .attr("x", function(d) {
+         return y(d.y);
+       })
+        .attr("dx", "6") // margin
+        .attr("dy", ".35em") // align vert di nuovo
+        .attr("transform", function(d) {
+         return "rotate(" + computeTextRotation(d) + ")";
+       })
+        //p
+        .append("foreignObject")
+        .attr('x', -width / 2)
+        .attr("width", width)
+        .attr("height", 200)
+        .append("xhtml:p")
+        .attr('style', 'word-wrap: break-word; text-align:center;')
+        .html('d.name');
+         el.remove();
+        };
+
+         d3.select(self.frameElement).style("height", height + "px");
+
+         d3.select(self.frameElement).style("height", height + "px");
+
+// SCALES INTERPOLATION
+function arcTween(d) {
+       var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
+       yd = d3.interpolate(y.domain(), [d.y, 1]),
+       yr = d3.interpolate(y.range(), [d.y ? 20 : 0, radius]);
+       return function(d, i) {
+         return i ? function(t) {
+           return arc(d);
+         } : function(t) {
+           x.domain(xd(t));
+           y.domain(yd(t)).range(yr(t));
+           return arc(d);
+         };
+       };
+     }
